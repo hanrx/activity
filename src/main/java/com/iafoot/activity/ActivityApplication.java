e34e0507470354761caa058fc94238f9ea6446d8
hanrx <1170807870@qq.com>
2020-11-12 11:04:52 +0800

    集成 activiti 添加 审批 demo，并添加页面demo 参考：https://github.com/jbarrez/spring-boot-with-activiti-example

--- src/main/java/com/iafoot/activity/ActivityApplication.java
+++ src/main/java/com/iafoot/activity/ActivityApplication.java
@@ -3,6 +3,12 @@ package com.iafoot.activity;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @Copyright: iAfoot.
+ * @Description:
+ * @author: RX.HAN
+ * @since: 2020/11/11 11:07 AM
+ */
 
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class ActivityApplication {
---
+++ src/main/java/com/iafoot/activity/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.iafoot.activity.controller;
+
+
+import com.iafoot.activity.entity.User;
+import com.iafoot.activity.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Created by yawn on 2018-01-11 10:02
+ */
+@RestController
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/getAllUser")
+    public Object getAllUser() {
+        return userService.getAllUser();
+    }
+
+    @GetMapping("/getAllGroup")
+    public Object getAllGroup() {
+        return userService.getAllGroup();
+    }
+
+    @GetMapping("/getUserGroup")
+    public Object getUserGroup(String groupId) {
+        return userService.getUserGroup(groupId);
+    }
+
+    @PostMapping("/addUser")
+    public Object addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+
+}
